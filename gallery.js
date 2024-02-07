219785108c40c1fc39a04917d819bf00ca3d1aae
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-02-07 09:47:03 -0300

    add funcao troca de imagem

--- gallery.js
+++ gallery.js
@@ -1,36 +1,59 @@
-async function galeriaImagens() {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=");
-  const data = await response.json();
+async function capturarPokemon() {
+  const pokemonResponse = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit="
+  );
+  const pokemonInformacao = await pokemonResponse.json();
+  const pokemonSelecionado = pokemonInformacao.results;
 
-  const imgPokemonClass = document.getElementsByClassName("imgPokemon");
-
-  data.results.forEach(async (pokemon, index) => {
-    const pokemonResponse = await fetch(pokemon.url);
-    const pokemonData = await pokemonResponse.json();
-
-    const frontDefault = pokemonData.sprites.front_default;
-    const backDefault = pokemonData.sprites.back_default;
-    const frontShiny = pokemonData.sprites.front_shiny;
-    const backShiny = pokemonData.sprites.back_shiny;
-
-    const images = [frontDefault, backDefault, frontShiny, backShiny];
-
-    const imgPokemon = imgPokemonClass[index];
-    imgPokemon.setAttribute("data-front-default", frontDefault);
-    imgPokemon.setAttribute("data-back-default", backDefault);
-    imgPokemon.setAttribute("data-front-shiny", frontShiny);
-    imgPokemon.setAttribute("data-back-shiny", backShiny);
-
-    const voltBotao = document.getElementById(`voltBotao`);
-    voltBotao.addEventListener("click", () => {
-      imgPokemon.src = images[index - 1];
-    });
-
-    const proxBotao = document.getElementById(`proxBotao`);
-    proxBotao.addEventListener("click", () => {
-      imgPokemon.src = images[index + 1];
-    });
+  pokemonSelecionado.forEach(async (pokemon) => {
+    const pokemonDataResponse = await fetch(pokemon.url);
+    const pokemonData = await pokemonDataResponse.json();
+    galeria(pokemonData);
   });
 }
 
-galeriaImagens();
+function galeria(pokemon) {
+  const divPokemon = document.querySelector(".pokeImages");
+  const frontDefault = pokemon.sprites.front_default;
+  const backDefault = pokemon.sprites.back_default;
+  const frontShiny = pokemon.sprites.front_shiny;
+  const backShiny = pokemon.sprites.back_shiny;
+
+  divPokemon.setAttribute("data-image-index", 0);
+  divPokemon.setAttribute("data-front-default", frontDefault);
+  divPokemon.setAttribute("data-back-default", backDefault);
+  divPokemon.setAttribute("data-front-shiny", frontShiny);
+  divPokemon.setAttribute("data-back-shiny", backShiny);
+}
+
+function trocarImagem(posicao, id) {
+  debugger;
+  const cardSelecionado = document.getElementById(id);
+  const cardSelecionadoInicio = Number(
+    cardSelecionado.getAttribute("data-image-index")
+  );
+
+  // Ele não está conseguindo selecionar a imagem aqui
+  const images = [
+    cardSelecionado.getAttribute("data-front-default"),
+    cardSelecionado.getAttribute("data-back-default"),
+    cardSelecionado.getAttribute("data-front-shiny"),
+    cardSelecionado.getAttribute("data-back-shiny"),
+  ];
+
+  let proximo = cardSelecionadoInicio + posicao;
+
+  if (proximo < 0) {
+    proximo = images.length - 1;
+  } else if (proximo >= images.length) {
+    proximo = 0;
+  }
+
+  debugger;
+  cardSelecionado.setAttribute("data-image-index", proximo);
+  const pokemonImage = cardSelecionado.querySelector(".imgPokemon");
+  pokemonImage.src = images[proximo];
+  debugger;
+}
+
+capturarPokemon();
