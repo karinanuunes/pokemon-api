6b04f159337194d7a5580497795b2fed48a75334
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-02-10 21:02:53 -0300

    arrumando bug imaggem

--- gallery.js
+++ gallery.js
@@ -12,18 +12,21 @@ async function capturarPokemon() {
   });
 }
 
+// acho que o erro acontece aqui
 function galeria(pokemon) {
-  const divPokemon = document.querySelector(".imgPokemon");
+  const divPokemons = document.querySelectorAll(".imgPokemon");
   const frontDefault = pokemon.sprites.front_default;
   const backDefault = pokemon.sprites.back_default;
   const frontShiny = pokemon.sprites.front_shiny;
   const backShiny = pokemon.sprites.back_shiny;
 
-  divPokemon.setAttribute("data-image-index", 0);
-  divPokemon.setAttribute("data-front-default", frontDefault);
-  divPokemon.setAttribute("data-back-default", backDefault);
-  divPokemon.setAttribute("data-front-shiny", frontShiny);
-  divPokemon.setAttribute("data-back-shiny", backShiny);
+  divPokemons.forEach((divPokemon) => {
+    divPokemon.setAttribute("data-image-index", 0);
+    divPokemon.setAttribute("data-front-default", frontDefault);
+    divPokemon.setAttribute("data-back-default", backDefault);
+    divPokemon.setAttribute("data-front-shiny", frontShiny);
+    divPokemon.setAttribute("data-back-shiny", backShiny);
+  });
 }
 
 function trocarImagem(posicao, id) {
@@ -41,16 +44,17 @@ function trocarImagem(posicao, id) {
 
   let proxIndex = cardSelecionadoInicio + posicao;
 
-  // tudo em cima está testado
   // não está entrando no IF
   if (proxIndex < 0) {
     proxIndex = images.length - 1;
   } else if (proxIndex >= images.length) {
     proxIndex = 0;
   }
+  console.log(proxIndex);
 
   cardSelecionado.setAttribute("data-image-index", proxIndex); // está dando undefined
   const pokemonImage = cardSelecionado.querySelector(".imgPokemon");
+  console.log(pokemonImage);
   pokemonImage.src = images[proxIndex]; // está dando null
 }
 
